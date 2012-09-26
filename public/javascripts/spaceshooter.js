7bc65aa6748f6e248e511325f687ec3cee92e324
Ville Anttonen <ville.anttonen@gmail.com>
2012-09-27 00:12:18 +0300

    basic keycontrols over ship

--- public/javascripts/spaceshooter.js
+++ public/javascripts/spaceshooter.js
@@ -9,17 +9,43 @@ var spaceshooter = (function (){
 
     var canvas = null,
         ctx = null,
-        items = [];
+        items = [],
+        ship = null;
 
         
     var Ship = function(x,y,rot){
        this.x = x;
        this.y = y;
-       this.angle = 0;;
+       this.mov_x = 0;
+       this.mov_y = 0;
+       this.angle = 0;
+       this.rotation = 0;
        
        this.move = function (){
-         this.angle += rot;
+         this.angle += this.rotation;
+         this.x += this.mov_x;
+         this.y += this.mov_y;
        };
+       
+       this.rotateLeft = function (){
+         this.rotation = -0.13;
+       }
+       
+       this.rotateRight = function (){
+         this.rotation = 0.13;
+       }
+       
+       this.forward = function (){
+         this.rotation = 0;
+         this.mov_x = Math.sin(this.angle) * 3;
+         this.mov_y = -Math.cos(this.angle) * 3;
+       }
+       
+       this.backward = function (){
+         this.rotation = 0;
+         this.mov_x = -Math.sin(this.angle) * 3;
+         this.mov_y = Math.cos(this.angle) * 3; 
+       }
        
        this.draw = function (ctx){
         ctx.save();
@@ -47,11 +73,26 @@ var spaceshooter = (function (){
         });
    };
    
+   
+   var initControls = function() {
+        $(window).keydown(function(e){
+        switch (e.keyCode)
+        {
+         case 37: ship.rotateLeft();break;
+         case 38: ship.forward();break;
+         case 39: ship.rotateRight();break;
+         case 40: ship.backward();break;
+        }
+      });
+   };
+   
    return { start : function (canvasId){
       canvas = $(canvasId)[0];
       ctx = canvas.getContext('2d');
-      items.push(new Ship(100,100,0.13));
-      items.push(new Ship(200,200,-0.03));
+      ship = new Ship(500,500,0.13)
+      items.push(ship);
+      items.push(new Ship(200,200,0));
+      initControls();
       setInterval(function (){
          repaint(ctx);
          _.each(items,function (item){
